62c421f751efb1e2b666f282ebe0e6991a97e851
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-05-07 14:26:37 +0200

    Closes #7

---
+++ back/api/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//Importation des modules
+const express = require('express');
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+
+// Crée une application Express
+const app = express();
+
+//Appel des routes automatisé
+const forumRoutes = path.join(__dirname,"./routeur/");
+fs.readdirSync(forumRoutes).forEach((file) => {
+    const route = require(path.join(forumRoutes, file));
+    app.use(route);
+})
+
+app.use(cors());
+app.use("/asset", express.static(path.join(__dirname,"../")));
+
+module.exports = app;
