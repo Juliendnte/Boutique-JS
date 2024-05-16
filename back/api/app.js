ff718e7a74dc9130bbd6635c892865c327feed79
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-05-16 16:47:37 +0200

    Closes #27

--- back/api/app.js
+++ back/api/app.js
@@ -4,21 +4,16 @@
 const express = require('express');
 const cors = require("cors");
 const path = require("path");
-const fs = require("fs");
 
 // Crée une application Express
 const app = express();
 app.use(cors());
-app.use(express.static('api'));
+app.use("/asset", express.static(path.join(__dirname,"./")));
 //Applique une limite de requête pour toutes les routes
 const rateLimit = require("./middlewares/rate-limit");
 app.use(rateLimit);
 
-//Appel des routes automatisé
-const forumRoutes = path.join(__dirname,"./routeur/");
-fs.readdirSync(forumRoutes).forEach((file) => {
-    const route = require(path.join(forumRoutes, file));
-    app.use(route);
-})
+const route = require("./routeur/articleRoute");
+app.use(route);
 
 module.exports = app;
