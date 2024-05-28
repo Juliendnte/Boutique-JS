335683b653286924230f0f488bb1b40aa83a5af5
Epikdimond-N <nathanael.pivot@ynov.com>
2024-05-28 12:11:45 +0200

    Closes #47

--- front/assets/js/script.js
+++ front/assets/js/script.js
@@ -6,6 +6,15 @@ const span3 = document.querySelector("#third-span");
 const burgerListCtn = document.querySelector(".burger-list-ctn");
 const burgerList = document.querySelectorAll(".burger-list-ctn p");
 const listP = document.querySelectorAll(".filtre p");
+const vidCtn = document.querySelector(".vid-ctn");
+const horlogerie = document.querySelector(".horlogerie");
+const burgerCategories = document.querySelectorAll(".burger-categories");
+const burgerListCtnSpan = document.querySelector(".burger-list-ctn span");
+const searchButton = document.querySelector(".search");
+const searchBar = document.querySelector(".search-bar");
+burgerCategories.forEach((cat) => {
+  cat.style = "display: none;";
+});
 
 function displayBurger() {
   span1.style =
@@ -13,9 +22,15 @@ function displayBurger() {
   span3.style =
     "transition: transform 0.5s; display: inline-block; transform-origin: left; transform: rotate(-45deg)";
   span2.style = "opacity: 0; transition: opacity 0.5s ease;";
-  burgerListCtn.style = "transition: 0.5s; height: 87vh;";
+  burgerListCtn.style = "transition: 0.5s; height: 87vh; overflow: hidden;";
+  burgerListCtnSpan.style = "display: flex";
   burgerList.forEach((elem) => {
-    elem.style = "opacity: 1; transition: opacity 1s ease;";
+    elem.style = "transition: opacity 1s ease;";
+  });
+  vidCtn.style = "transition: 0.7s; display: none;";
+  horlogerie.style = "transition: 0.7s; display: none;";
+  burgerCategories.forEach((cat) => {
+    cat.style = "display: flex";
   });
   burger.removeEventListener("click", displayBurger);
   burger.addEventListener("click", hideBurger);
@@ -27,10 +42,32 @@ function hideBurger() {
   span3.style =
     "transition: transform 0.5s; display: inline-block; transform-origin: left; transform: rotate(0deg)";
   span2.style = "opacity: 1; transition: opacity 0.5s ease;";
-  burgerListCtn.style = "transition: 0.5s; height: 0;";
+  burgerListCtn.style = "transition: 0.5s; height: 0px;";
   burgerList.forEach((elem) => {
-    elem.style = "opacity: 0; transition: opacity 0.2s ease;";
+    elem.style = "transition: opacity 0.2s ease;";
+  });
+  burgerListCtnSpan.style = "display: none";
+  vidCtn.style =
+    "display: flex; align-items: center; justify-content: center; overflow: hidden;";
+  horlogerie.style =
+    "display: flex; align-items: center; justify-content: center; overflow: hidden;";
+  burgerCategories.forEach((cat) => {
+    cat.style = "display: none";
   });
   burger.removeEventListener("click", hideBurger);
   burger.addEventListener("click", displayBurger);
+}
+
+searchButton.addEventListener("click", displaySearch);
+
+function displaySearch() {
+  searchBar.style = "height: 70px; transition: all 0.3s;";
+  searchButton.removeEventListener("click", displaySearch);
+  searchButton.addEventListener("click", hideSearch);
+}
+
+function hideSearch() {
+  searchBar.style = "height: 0vh; transition: all 0.3s;";
+  searchButton.removeEventListener("click", hideSearch);
+  searchButton.addEventListener("click", displaySearch);
 }
